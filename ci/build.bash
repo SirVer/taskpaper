0946dec5b3592c2ac437bcb049857ab94495d73f
Holger Rapp <HolgerRapp@gmx.net>
2021-05-23 21:33:50 +0200

    Should be it.

--- ci/build.bash
+++ ci/build.bash
@@ -15,8 +15,8 @@ required_arg $CROSS 'CROSS'
 required_arg $TARGET_TRIPLE '<Target Triple>'
 
 if [ -z "$RELEASE_BUILD" ]; then
-    $CROSS build --target $TARGET_TRIPLE
-    $CROSS build --target $TARGET_TRIPLE --all-features
+    $CROSS build --all --target $TARGET_TRIPLE
+    $CROSS build --all --target $TARGET_TRIPLE --all-features
 else
-    $CROSS build --target $TARGET_TRIPLE --all-features --release
+    $CROSS build --all --target $TARGET_TRIPLE --all-features --release
 fi
